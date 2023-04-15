3cd687ea07ba266d4813ec4a178d419144c989d6
krish-1712 <krishkannan1712@gmail.com>
2023-04-15 10:30:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// import  express  from "express";
+// import * as fs from 'fs';
+const express = require("express")
+const fs= require("fs")
+const path=require("path");
+
+
+const dirPath= path.join(__dirname , "timestamps");
+console.log(dirPath)
+
+const app=express()
+
+
+app.use(express.static("timestamps"))
+ 
+app.get("/static",(req,res)=>{
+    let time= new Date()
+let dateString= time.toUTCString().slice(0,-4)
+console.log(dateString);
+
+
+const timeStamp =`Last Created TimeStamp : ${dateString}`
+
+    fs.writeFileSync(`${dirPath}/date-time.txt`,timeStamp, (err)=>{
+        if(err){
+       console.log(err)
+        }else{
+            console.log("file created Successfully")
+        }
+     
+})
+    res.sendFile(path.join(__dirname,"timestamps/date-time.txt"));
+})
+
+app.get("/",(req,res)=>{
+    res.send("hi how are u?")
+})
+
+app.listen(5000,()=> console.log(`server started in localhost:5000`))
